04381886028955a2492f0642fc8ec2bb26717aa7
Leopold Bauer <Leopold.Bauer@titanom.com>
2025-04-21 17:31:35 +0200

    fix: lint

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -18,7 +18,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
 import { Alert, AlertDescription } from '@/components/ui/alert';
-import { Gamepad } from 'lucide-react';
 
 const JoinGameFormSchema = z.object({
   gameCode: z
