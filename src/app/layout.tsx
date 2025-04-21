00082bc8e44bfadf5dc8e12724243b3b60e92455
Leopold Bauer <Leopold.Bauer@titanom.com>
2025-04-21 18:37:02 +0200

    fix: status bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className='w-[100dvw] h-[100dvh] overflow-hidden'>
+      <Head>
+        <meta name="theme-color" content="black" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
       <body className={inter.className}>
         {children}
       </body>
