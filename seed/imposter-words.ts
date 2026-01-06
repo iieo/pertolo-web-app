c29fd3266ce73cad7e554dab210fe03d80f54870
Leopold Bauer <Leopold.Bauer@titanom.com>
2026-01-06 14:38:40 +0100

    feat: seeding

---
+++ seed/imposter-words.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import fs from "fs";
+import { db } from "@/db";
+import { imposterCategoriesTable, impostorWordsTable } from "@/db/schema";
+
+
+async function seedImpostorWords() {
+    const data = fs.readFileSync(path.join(__dirname, 'imposter-words.json'), 'utf-8');
+
+    const jsonObject = JSON.parse(data);
+
+    for (const key of Object.keys(jsonObject)) {
+        const wordlist = jsonObject[key];
+        // You can process wordlist here
+        const [insertedCategory] = await db.insert(imposterCategoriesTable).values({
+            name: key,  
+        }).returning();
+
+        if (!insertedCategory) {
+            throw new Error(`Failed to insert category: ${key}`);
+        }
+        
+        const categoryId = insertedCategory.id;
+
+        await Promise.all(wordlist.map(async (word: string) => {
+            await db.insert(impostorWordsTable).values({
+                word,
+                categoryId,
+            });
+        }));
+    }
+}
+
+seedImpostorWords().then(() => {
+    console.log("Seeding completed.");
+}).catch((error) => {
+    console.error("Error during seeding:", error);
+});
